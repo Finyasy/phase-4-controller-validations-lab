51babbbad3fecb9199e71cf66cf1c3fa6f159828
Bryan Bosire <bryanbosire@gmail.com>
2022-10-04 13:08:31 +0300

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ApplicationRecord
+    # Add validations to Author such that...
+    # The name cannot be blank
+    validates :name, presence: true
+    # The e-mail is unique
+    validates :email, uniqueness: true
 end
